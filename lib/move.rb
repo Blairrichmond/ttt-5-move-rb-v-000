6917cfaec8c976a918337b3c8e82e9db4756b5f7
Brent  Ransom <sadumedusa@gmail.com>
2017-10-13 15:55:02 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input - 1)
+def input_to_index(user_input)
   '5'.to_i
   expect(user_input).to eq(-1)
 end
